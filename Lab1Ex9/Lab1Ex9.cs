9e995e04625dfb28262f1032df413030d7d99c1f
Dani <amaximoaiei.l@yahoo.com>
2023-06-24 21:46:17 +0300

    Add project files.

---
+++ Lab1Ex9/Lab1Ex9.cs
@@ -0,0 +1,45 @@
+﻿/*Ex 9
+Folosind instructiunea “switch”, scrieti un program care citind de la tastatura un numar
+intreg va verifica urmatoarele
+“unu” daca numarul citit este 1
+“doi” daca numarul citit este 2
+“trei” daca numarul citit este 3
+“cinci” daca numarul citit este 5
+“opt” daca numarul citit este 8
+“neidentificat” pentru orice alt caz*/
+
+class Lab1Ex9
+{
+
+    static void Main()
+    {
+
+        int numarTastatura;
+
+        Console.WriteLine("Introduceti numarul:");
+        numarTastatura = int.Parse(Console.ReadLine());
+
+        switch (numarTastatura)
+        {
+            case 1:
+                Console.WriteLine("Unu");
+                break;
+            case 2:
+                Console.WriteLine("Doi");
+                break;
+            case 3:
+                Console.WriteLine("Trei");
+                break;
+            case 5:
+                Console.WriteLine("8");
+                break;
+            default:
+                Console.WriteLine("Neidentificat");
+                break;
+        }
+    }
+}
+
+
+
+
